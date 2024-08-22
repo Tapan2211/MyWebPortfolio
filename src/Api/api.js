f9cd948124edd7eca57a0099b71a7bbaafa69e38
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-08-22 12:14:51 +0530

    add code splitting and lazy loading for this app

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 // const BaseURL = 'http://localhost:8082';
 const BaseURL = 'https://mywebportfoliobackend.onrender.com';
+// const BaseURL = process.env.REACT_APP_API_BASE_URL;
+
 
 export const getSkill = async (obj) => {
     try {
--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import  Home  from './components/Home/Home';
+// import Home from './components/Home/Home';
+
+const Home = React.lazy(() => import('./components/Home/Home'));
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Routes>
-
-            <Route exact path='/' element={<Home />}/>
-          </Routes>
-        </div>
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <div>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+            </Routes>
+          </div>
+        </Router>
+      </Suspense>
     </div>
   );
 }
